f21b08d7df4769a32cb8160eb9290a9fe39355f3
Nyd Iokar <nydiokar@gmail.com>
2024-11-30 23:13:01 +0200

    added functionalities and tests

---
+++ src/multi_agent_orchestrator/adapters/discord/__init__.py
@@ -0,0 +1,4 @@
+from .adapter import DiscordAdapter
+from multi_agent_orchestrator.types import DiscordAdapterConfig, ProcessedContent, DiscordAttachment
+
+__all__ = ['DiscordAdapter', 'DiscordAdapterConfig', 'ProcessedContent', 'DiscordAttachment']
---
+++ src/tests/conftest.py
@@ -0,0 +1,5 @@
+# src/tests/conftest.py
+import os
+import sys
+# Add the src directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
